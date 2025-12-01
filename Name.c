4683934faef3e33870382ec35029f531b16ffb9a
AliAnsari2025 <mdaliansari@eng.rizvi.edu.in>
2025-12-01 09:22:54 +0530

    Create Name.c

---
+++ Name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    /*
+    Name= Ali Ansari
+    Div= C(1)
+    Age= 18
+    UIN= 251P011
+    Class= Computer Engineer FE
+    */
+    
+    printf("Name= Ali Ansari \n");
+    printf("Div= C(1) \n");
+    printf("Age= 18 \n");
+    printf("UIN= 251P011 \n");
+    printf("Class= Computer Engineer FE \n");
+    
+    return 0;
+}
